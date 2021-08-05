8a2b5b8c862d7d1dc1225d3fb1e581644a9680dd
wmcconvi <48520454+wmcconvi@users.noreply.github.com>
2021-08-05 14:18:40 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template
+from datetime import date, datetime
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    name = request.args.get("name", "world")
+    return render_template("home.html", name=name)
+
+
+@app.route("/agecalc", methods=["POST"])
+def compute_age():
+    name = request.form.get('name')
+    birthday = request.form.get('birthday')
+    birthdayObject = datetime.strptime(birthday, "%Y-%m-%d")
+    currentDate = date.today()
+    age = (currentDate.year - birthdayObject.year) - ((currentDate.month,
+                                                       currentDate.day) < (birthdayObject.month, birthdayObject.day))
+    return render_template("profile.html",
+                           age=age,
+                           name=name,
+                           birthday=birthday)
